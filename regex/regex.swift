05c2a545a3da514929f66ef5054adf3a79d28cbf
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-08-22 15:44:47 +0000

    testing out regex in swift

---
+++ regex/regex.swift
@@ -0,0 +1,9 @@
+import Foundation
+let test_string = "hat"
+let range = NSRange(location: 0, length: test_string.utf16.count)
+let regex = try! NSRegularExpression(pattern: "[a-z]at")
+
+//print(regex.firstMatch(in: test_string, options: [], range: range) ?? "nothing found")
+regex.firstMatch(in: test_string, options: [], range: range)
+
+    
