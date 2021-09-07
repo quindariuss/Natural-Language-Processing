9cc4cf6cde23d6d9c7760937312f53a766d92859
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-09-07 20:07:19 +0000

    adding some files

---
+++ similarity_distance/matrix.js
@@ -0,0 +1,7 @@
+sentence_one = "Spokesman confirms"
+sentence_two = "Spokewoman said"
+var distance_matrix = new Array(18)
+for(const index = 0; index < distance_matrix; index++){
+	distance_matrix[index] = new Array(18)
+}
+console.log(distance_matrix)
