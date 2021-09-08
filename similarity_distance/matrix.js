3fba21fa413526957a98351d58b0c48e2acd9e29
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-09-08 10:29:44 -0400

    restarting

--- similarity_distance/matrix.js
+++ similarity_distance/matrix.js
@@ -1,7 +1,4 @@
 sentence_one = "Spokesman confirms"
 sentence_two = "Spokewoman said"
-var distance_matrix = new Array(18)
-for(const index = 0; index < distance_matrix; index++){
-	distance_matrix[index] = new Array(18)
-}
-console.log(distance_matrix)
+sentence_two = sentence_two + "a"
+console.log(sentence_two)
