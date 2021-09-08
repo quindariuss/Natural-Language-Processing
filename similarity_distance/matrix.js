94fcdd77618289c945586946ed4b8ce7cd034802
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-09-08 12:02:09 -0400

    the matrix is begin printed out correctly

--- similarity_distance/matrix.js
+++ similarity_distance/matrix.js
@@ -1,7 +1,43 @@
-sentence_one = "Spokesman confirms"
-sentence_two = "Spokewoman said"
-for (var index = 0; index < sentence_one.length(); index++)
+sentence_one = "  Spokesman confirms"
+sentence_two = "  Spokewoman said"
+var Matrix = new Array(sentence_one.length)
+for (var index = 0; index < sentence_one.length; index++)
 {
-	console.log(sentence_one[index])
+	Matrix[index] = new Array(sentence_two.length)
+	Matrix[index][0] = sentence_one[index]
+}
+for (var index = 0; index < sentence_two.length; index++)
+{
+	Matrix[0][index] = sentence_two[index]
 }
 
+for(var index = 1; index < sentence_one.length; index++)
+{
+	for(var subindex = 1; subindex < sentence_two.length; subindex++)
+	{
+		Matrix[index][subindex] = 0
+	}
+}
+for(var index = 0; index < sentence_one.length; index++)
+{
+	for(var subindex = 0; subindex < sentence_two.length; subindex++)
+	{
+		process.stdout.write(" " + Matrix[index][subindex])
+	}
+	process.stdout.write("\n")
+}
+
+function edit_distance(array_one, array_two)
+{
+	count = 0; 
+	for (var index = 0; index < array_one.length; index++) 
+	{
+		if(array_one[index] != array_two[index])
+		{
+			count++
+		}
+	}
+	return count;
+}
+
+console.log(edit_distance(sentence_one,sentence_two))
