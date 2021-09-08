824a4be64b63d53dcc7674a4290f4a886cd64515
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-09-08 10:31:05 -0400

    restarting

--- similarity_distance/matrix.js
+++ similarity_distance/matrix.js
@@ -1,4 +1,7 @@
 sentence_one = "Spokesman confirms"
 sentence_two = "Spokewoman said"
-sentence_two = sentence_two + "a"
-console.log(sentence_two)
+for (var index = 0; index < sentence_one.length(); index++)
+{
+	console.log(sentence_one[index])
+}
+
