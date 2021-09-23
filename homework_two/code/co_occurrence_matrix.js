7b02d1bf598d2b89bfdee2ca461bf5e364c4ebb6
Quin'darius Ali Lyles-Woods <quinwoodz@gmail.com>
2021-09-23 16:39:42 -0400

    Finished with everything but question four

--- homework_two/code/co_occurrence_matrix.js
+++ homework_two/code/co_occurrence_matrix.js
@@ -1,60 +1,128 @@
-const array_one = 
+const vocabulary = 
 	[
+		"is",
+		"going",
+		"to",
+		"today",
 		"I",
-		"like",
-		"enjoy",
-		"deep",
-		"learning",
+		"am",
 		"NLP",
-		"flying",
-		".",
+		"topic",
+		"It",
+		"rain",
+		"not",
+		"outside",
+		"an",
+		"interesting",
+		"includes",
+		"ML",
+		"DL",
+		"complete",
+		"homework",
 	]
 
-const array_two = 
+
+/*const sentence = 
+	[
+		"NLP",
+		"is",
+		"an",
+		"interesting",
+		"topic",
+	]*/
+
+/*const sentence = 
+	[
+		"NLP",
+		"includes",
+		"ML",
+		"DL",
+		"topics",
+		"too",
+	]*/
+/*const sentence = 
 	[
 		"I",
-		"like",
-		"enjoy",
-		"deep",
-		"learning",
+		"am",
+		"going",
+		"to",
+		"complete",
 		"NLP",
-		"flying",
-		".",
-	]
-
-
+		"homework",
+		"today",
+	]*/
 const sentence = 
 	[
-		"I",
-		"like",
-		"deep",
-		"learning",
-		".",
-		"I",
-		"like",
-		"NLP",
-		"I",
-		"enjoy",
-		"flying",
-		".",
+		"It",
+		"is",
+		"going",
+		"to",
+		"rain",
+		"today",
 	]
-
 function context (word_one, word_two, context){
 let count = 0
 var array_occurences = new Array 	
 	for(index = 0; index < sentence.length; index ++){
-		if (sentence[index] == word_one)
+		if (sentence[index] == word_two)
 		{
 			array_occurences.push(index)
 		}
 	}
-	console.log({array_occurences})
 for(index = 0; index < array_occurences.length; index++){
-	if (sentence[array_occurences[index] + 1] == word_two)
+	if (array_occurences[index] + context < sentence.length){
+	if (sentence[array_occurences[index] + context] == word_one)
 		count++
-}
+	}
+	if (array_occurences[index] + context - 1 < sentence.length){
+	if (sentence[array_occurences[index] + context - 1] == word_one)
+		count++
+	}
+	if (array_occurences[index] + context - 2< sentence.length){
+	if (sentence[array_occurences[index] + context - 2] == word_one)
+		count++
+	}
+	if (array_occurences[index] - context > 0){
+	if (sentence[array_occurences[index] - context] == word_one)
+		count++
+	}
+	if (array_occurences[index] - context + 1 > 0){
+	if (sentence[array_occurences[index] - context + 1] == word_one)
+		count++
+	}
+	if (array_occurences[index] - context + 2> 0){
+	if (sentence[array_occurences[index] - context + 2] == word_one)
+		count++
+	}
+	}
 return count
 }
-
-console.log(context("I","like", 1))
-
+for (let index = 0; index < sentence.length; index++){
+           process.stdout.write(sentence[index] + "&")
+}
+console.log("\nCo-occurrence matrix\n")
+for (let index = 0; index < vocabulary.length; index++){
+	process.stdout.write(vocabulary[index] + "&")
+	for (let subindex = 0; subindex < sentence.length; subindex++){
+process.stdout.write((context(vocabulary[index],sentence[subindex], 3).toString() + "&"))
+	}
+process.stdout.write("\n")
+}
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
+//console.log((context(vocabulary[index],sentence[index], 3).toString() + "&"))
