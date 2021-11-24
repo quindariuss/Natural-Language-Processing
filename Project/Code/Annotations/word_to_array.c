b6179b8dd9c28347bd132b859529eb1290cc76de
Quin'darius Ali Lyles-Woods <quindarius@quindariuss-mbp.lan>
2021-11-24 17:21:02 -0500

    Adding some major project files

--- Project/Code/Annotations/word_to_array.c
+++ Project/Code/Annotations/word_to_array.c
@@ -1,23 +1,19 @@
 #include <stdio.h> 
 #include <stdlib.h>
-int main() 
-{
-	int index; 
-	int word_count = 10000;
-	char * word = (char*)malloc(sizeof(char) * 40);
 
-	while((scanf("%s",word) != EOF))
-	{
-		word_count++; 
-	}
-	char words[word_count][50];
-	for (index = 0; index < word_count; index++)
-	{
-		scanf("%s", words[index]);
-	}
+ int main(int argc, char *argv[])
+ {
+ 	int index;
+ 	int word_count = 5;
+ 	char words[word_count][45];
 
-	for (index = 0; index < word_count; index++)
-	{
-		printf("%s\n", words[index]);
-	}
-}
+ 	for (index = 0; index < word_count; index++)
+ 	{
+ 		scanf("%s", words[index]);
+ 	}
+
+ 	for (index = 0; index < word_count; index++)
+ 	{
+ 		printf("%s\n", words[index]);
+ 	}
+ }
